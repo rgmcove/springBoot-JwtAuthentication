c739dbb716050716cec4fbd606316b05d508636f
rago.mos <ralberto115@gmail.com>
2024-10-17 19:23:55 -0500

    build: new and configuration project

---
+++ src/test/java/co/com/ragomos/SpringBootJwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.ragomos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
